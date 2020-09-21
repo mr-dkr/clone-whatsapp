00bf802afd9e5124550c46bf2a88eec20e15cc6f
Divakar <Divakarthambidurai@gmail.com>
2020-09-21 10:14:39 +0100

    Frontend & Foundation of Application

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,23 @@
 import { Avatar, IconButton } from '@material-ui/core'
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React, { useState, useEffect } from 'react'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
 import './Chat.css'
+import MicIcon from '@material-ui/icons/Mic'
 
 function Chat() {
+    const [input, setInput] = useState('')
     const [seed, setSeed] = useState('');
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
 
     }, [])
+
+    const sendMessage=(e)=>{
+        e.preventDefault();
+        console.log("You Types >>", input)
+    }
 
     return (
         <div className="chat">
@@ -21,26 +29,33 @@ function Chat() {
                 </div>
                 <div className="chat__headerRight">
                     <IconButton>
-                        <SearchOutlined/>
+                        <SearchOutlined />
                     </IconButton>
                     <IconButton>
-                        <AttachFile/>
+                        <AttachFile />
                     </IconButton>
                     <IconButton>
-                        <MoreVert/>
+                        <MoreVert />
                     </IconButton>
                 </div>
 
             </div>
             <div className="chat__body">
                 <p className={`chat__message ${true && "chat__reciever"}`}>
-                <span className="chat__name"> Divakar </span>
+                    <span className="chat__name"> Divakar </span>
                     Hey Guys, How you doing?
                     <span className="chat__timestamp">12.00AM</span>
                 </p>
 
             </div>
             <div className="chat__footer">
+                    <InsertEmoticonIcon />
+
+                <form action="">
+                    <input value={input}  onChange = {e => setInput(e.target.value)} type="text" placeholder="Type a message" />
+                    <button onClick={sendMessage} type="submit">Send a message</button>
+                </form>
+                    <MicIcon />
 
             </div>
         </div>
